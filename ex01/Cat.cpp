20274958d2e3df676d8f4e0200dda86d9796d5f1
Pastifier <136130975+Pastifier@users.noreply.github.com>
2024-09-01 14:38:51 +0400

    Saved a very unfortunate situation where a leak happens when the assignment-operator overload is called

--- ex01/Cat.cpp
+++ ex01/Cat.cpp
@@ -37,6 +37,7 @@ Cat::Cat(const Cat& other) : Animal() {
 			_cogito = NULL;
 			DEBUG_PRINT("WARNING: Field `_cogito` is pointing to `NULL`");
 		} else {
+			_cogito = new Brain();
 			*_cogito = *(other._cogito);
 		}
 	} catch (const std::exception& e) {
@@ -60,7 +61,6 @@ Cat& Cat::operator=(const Cat& rhs) {
 			_cogito = NULL;
 			DEBUG_PRINT("WARNING: Field `_cogito` is pointing to `NULL`");
 		} else {
-			_cogito = new Brain();
 			*_cogito = *(rhs._cogito);
 		}
 	} catch (const std::exception& e) {
