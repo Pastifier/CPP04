42e286c12dad1cb7cdcf19125bcc57b59b8ec83a
Emran Binjamaan <therealemran.design@gmail.com>
2024-11-23 22:20:14 +0400

    Initial commit.

---
+++ ex03/AMateria.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+
+class ICharacter;
+
+class AMateria
+{
+protected:
+//[...]
+public:
+	AMateria(std::string const & type);
+//[...]
+	std::string const & getType() const; //Returns the materia type
+	virtual AMateria* clone() const = 0;
+	virtual void use(ICharacter& target);
+};
